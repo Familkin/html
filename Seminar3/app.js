f057c51f9e0f1e523c1208c726c4b261a38b78e5
Familkin <cepjil@mail.ru>
2023-02-23 21:30:19 +0300

    seminar 3

--- Seminar3/app.js
+++ Seminar3/app.js
@@ -1,14 +1,15 @@
-let resilt = 158+2;
-
-alert(`результат: ${resilt}`);
-
+// let resilt = 158+2;
+// alert(`результат: ${resilt}`);
 
 
+<script language="JavaScript">
+        let right = confirm("Ты уверен?");
+    </script>
 
+// let name = prompt("Введите имя", "Введите имя сюда")
 
+// function functionWell() {
+//     alert(`Привет, ${name}`)
+// }
 
-function functionWell() {
-    console.log(23);
-
-
-}
+// functionWell()
